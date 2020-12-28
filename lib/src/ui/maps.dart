d63cba19cf3e8183ec0a62c75a63ec342d89cbdd
Arif R <arifrhmn2743@gmail.com>
2020-12-28 10:32:34 +0700

    Maps Updated

--- lib/src/ui/maps.dart
+++ lib/src/ui/maps.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapsPage extends StatefulWidget {
   @override
@@ -7,15 +8,40 @@ class MapsPage extends StatefulWidget {
 }
 
 class _MapsPageState extends State<MapsPage> {
+  LatLng _initialcameraposition = LatLng(-6.3932405, 106.7957506);
+  GoogleMapController _controller;
+  Location _location = Location();
+
+  void _onMapCreated(GoogleMapController _cntlr) {
+    _controller = _cntlr;
+    _location.onLocationChanged.listen((l) {
+      _controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: LatLng(l.latitude, l.longitude), zoom: 11),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Maps Demo'),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition:
+                  CameraPosition(target: _initialcameraposition),
+              mapType: MapType.normal,
+              onMapCreated: _onMapCreated,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+            )
+          ],
         ),
-        body: GoogleMap(
-          initialCameraPosition:
-              CameraPosition(target: LatLng(40.688841, -74.044015), zoom: 11),
-        ));
+      ),
+    );
   }
 }
