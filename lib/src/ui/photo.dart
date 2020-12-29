bdcd7f301aadf275420691b3f5143af18b46d90a
Arif R <arifrhmn2743@gmail.com>
2020-12-29 10:49:41 +0700

    pick photo done

--- lib/src/ui/photo.dart
+++ lib/src/ui/photo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PhotoPage extends StatefulWidget {
   @override
@@ -6,12 +7,66 @@ class PhotoPage extends StatefulWidget {
 }
 
 class _PhotoPageState extends State<PhotoPage> {
+  String _path = null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Take Photo'),
       ),
+      body: SafeArea(
+          child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset("images/p.jpg"),
+          ),
+          FlatButton(
+            onPressed: () {
+              _showOptions(context);
+            },
+            child: Text("Take Picture", style: TextStyle(color: Colors.white)),
+            color: Colors.blueAccent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          )
+        ],
+      )),
     );
+  }
+
+  void _showOptions(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            height: 120,
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.photo_camera),
+                  title: Text("Take a photo from camera"),
+                ),
+                ListTile(
+                  leading: Icon(Icons.photo_library),
+                  title: Text("Choose from gallery"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    _showPhotoLibrary();
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  void _showPhotoLibrary() async {
+    final file = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _path = file.path;
+    });
   }
 }
