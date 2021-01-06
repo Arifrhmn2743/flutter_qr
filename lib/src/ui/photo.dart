f8549d4214b63c1277e107f8f99870922ab0690d
Arif rahman <arifrhmn2743@gmail.com>
2021-01-06 12:28:47 +0700

    camera working now

--- lib/src/ui/photo.dart
+++ lib/src/ui/photo.dart
@@ -12,64 +12,9 @@ class PhotoPage extends StatefulWidget {
 class _PhotoPageState extends State<PhotoPage> {
   String _path = null;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Take Photo'),
-      ),
-      body: SafeArea(
-          child: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            _path == null
-                ? Image.asset("images/p.jpg")
-                : Image.file(File(_path)),
-            FlatButton(
-              onPressed: () {
-                _showOptions(context);
-              },
-              child:
-                  Text("Take Picture", style: TextStyle(color: Colors.white)),
-              color: Colors.blueAccent,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            )
-          ],
-        ),
-      )),
-    );
-  }
-
-  void _showOptions(BuildContext context) {
-    showModalBottomSheet(
-        context: context,
-        builder: (context) {
-          return Container(
-            height: 120,
-            child: Column(
-              children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.photo_camera),
-                  title: Text("Take a photo from camera"),
-                ),
-                ListTile(
-                  leading: Icon(Icons.photo_library),
-                  title: Text("Choose from gallery"),
-                  onTap: () {
-                    Navigator.pop(context);
-                    _showPhotoLibrary();
-                  },
-                )
-              ],
-            ),
-          );
-        });
-  }
-
   void _showPhotoLibrary() async {
     final file = await ImagePicker.pickImage(source: ImageSource.gallery);
+
     setState(() {
       _path = file.path;
     });
@@ -83,8 +28,51 @@ class _PhotoPageState extends State<PhotoPage> {
         context,
         MaterialPageRoute(
             builder: (context) => TakePicturePage(camera: camera)));
+
     setState(() {
       _path = result;
     });
+  }
+
+  void _showOptions(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+              height: 150,
+              child: Column(children: <Widget>[
+                ListTile(
+                    onTap: () {
+                      Navigator.pop(context);
+                      _showCamera();
+                    },
+                    leading: Icon(Icons.photo_camera),
+                    title: Text("Take a picture from camera")),
+                ListTile(
+                    onTap: () {
+                      Navigator.pop(context);
+                      _showPhotoLibrary();
+                    },
+                    leading: Icon(Icons.photo_library),
+                    title: Text("Choose from photo library"))
+              ]));
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Column(children: <Widget>[
+        _path == null ? Image.asset("images/p.jpg") : Image.file(File(_path)),
+        FlatButton(
+          child: Text("Take Picture", style: TextStyle(color: Colors.white)),
+          color: Colors.green,
+          onPressed: () {
+            _showOptions(context);
+          },
+        )
+      ]),
+    ));
   }
 }
